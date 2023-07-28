6b77db519ce614481e8434733f74889bbfc4c838
vbnet3d <ddzimianski@gmail.com>
2023-07-28 15:36:26 -0400

    Add project files.

---
+++ Harmony/Init.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace SpawnSleepersInRange.Harmony
+{
+    internal class Init : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Harmony/SleeperVolumeSpawn.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace SpawnSleepersInRange.Harmony
+{
+    [HarmonyPatch(typeof(global::SleeperVolume))]
+    [HarmonyPatch("Spawn")]
+    public class SleeperVolumeSpawn
+    {
+        public static void Postfix(ref EntityAlive __result, int ___flags)
+        {
+            __result.SetSleeperActive();
+            __result.ResumeSleeperPose();
+        }
+    }
+
+    [HarmonyPatch(typeof(global::SleeperVolume))]
+    [HarmonyPatch("Tick")]
+    public class SleeperVolumeTick
+    {
+        private static MethodInfo touchGroup;
+
+        public static void Postfix(SleeperVolume __instance, World _world)
+        {
+            if (!__instance.wasCleared)
+            {
+                if (touchGroup == null)
+                {
+                    touchGroup = typeof(SleeperVolume).GetMethod("TouchGroup", BindingFlags.Instance | BindingFlags.NonPublic);
+                }
+
+                foreach (EntityPlayer player in _world.Players.list)
+                {
+                    if (PlayerWithinRange(__instance, player, 30.0f))
+                    {
+                        touchGroup.Invoke(__instance, new object[] { _world, player, false });
+                    }
+                }
+            }
+        }
+
+        private static bool PlayerWithinRange(SleeperVolume volume, EntityPlayer player, float range)
+        {
+            return Vector3.Distance(volume.Center, player.position) <= range;
+        }
+    }    
+}
