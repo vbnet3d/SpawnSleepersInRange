c9d1c9fe95da3b2361b95da377516877674125c4
vbnet3d <ddzimianski@gmail.com>
2025-06-19 14:55:17 -0400

    Overhaul spawning methodology to improve performance in large POIs

--- Harmony/SpawnManager.cs
+++ Harmony/SpawnManager.cs
@@ -4,27 +4,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
 namespace SpawnSleepersInRange.Harmony
 {
+    public class POISpawnTracking
+    {
+        public DateTime LastPlayerInPOI { get; set; }
+        public EntityPlayer MostRecentPlayer { get; set; }
+        public PrefabInstance POIPrefab { get; set; }
+        public List<SleeperVolume> SleeperVolumes { get; set; }
+        public Queue<TriggerVolume> TriggerVolumes { get; set; }
+    }
+
     public class SpawnManager : MonoBehaviour
     {
         private static SpawnManager instance;
-
-        private Dictionary<EntityPlayer, Queue<SleeperVolume>> toSpawn;
-        private Dictionary<EntityPlayer, Queue<TriggerVolume>> toTrigger;
-        private Dictionary<EntityPlayer, PrefabInstance> poiTracker;
+        private List<POISpawnTracking> poiSpawnTracking = new List<POISpawnTracking>();
 
         public bool IsRunning { get; private set; }
-
+        
         public SpawnManager()
-        {
-            toSpawn = new Dictionary<EntityPlayer, Queue<SleeperVolume>>();
-            toTrigger = new Dictionary<EntityPlayer, Queue<TriggerVolume>>();
-            poiTracker = new Dictionary<EntityPlayer, PrefabInstance>();
+        {           
         }
 
         public static SpawnManager Instance
@@ -43,7 +47,7 @@ namespace SpawnSleepersInRange.Harmony
         public void Start()
         {
             IsRunning = true;
-            StartCoroutine(SpawnHandler());
+            StartCoroutine(SpawnCheckLoop());
         }
 
         public void Stop()
@@ -52,115 +56,146 @@ namespace SpawnSleepersInRange.Harmony
             StopAllCoroutines();
         }
 
-        IEnumerator SpawnHandler()
+        /*
+         Update algorithm:
+            - For each player check if they are in the bounds of a POI.
+            - If they are, check if the POI is already tracked for spawns and triggers. If POI is already cleared, do nothing.
+            - If not, cache collection of sleeper volumes and queue for trigger volumes for that POI sorted by distance from the player. 
+                Exclude all that are within the boundaries of a Land Claim Block
+            - For sleeper volumes, do distance check in a coroutine against the player position and the volume center. Spawn if within range.
+            - For trigger volumes, run through a coroutine and trigger them sequentially with a delay.
+            - If no player has been in a POI for > 30 seconds, clear the queues and collections for that POI.
+         */
+
+        IEnumerator SpawnCheckLoop()
         {
-            while (true)
+            while (IsRunning)
             {
                 try
                 {
-                    foreach (var player in GameManager.Instance.World.Players.list)
+                    QueueNewPOIs();
+
+                    // check for POIs that have not been visited by a player in the last 30 seconds
+                    foreach (var poi in poiSpawnTracking)
                     {
-                        if (player.prefab == null)
+                        if (poi.LastPlayerInPOI < DateTime.Now.AddSeconds(-30))
                         {
-                            if (toSpawn.ContainsKey(player))
+                            foreach (var sleeperVolume in poi.SleeperVolumes)
                             {
-                                toSpawn[player].Clear();
-                                toSpawn.Remove(player);
+                                sleeperVolume.Reset();
                             }
-
-                            if (toTrigger.ContainsKey(player))
-                            {
-                                toTrigger[player].Clear();
-                                toTrigger.Remove(player);
-                            }
-
-                            poiTracker.Remove(player);
-                        }
-                        else if (toSpawn.ContainsKey(player))
-                        {
-                            if (poiTracker.ContainsKey(player) && poiTracker[player] == player.prefab)
-                            {
-                                var sleeperVolume = toSpawn[player].Dequeue();
-                                sleeperVolume.TouchGroup(GameManager.Instance.World, player, false);
-
-                                var triggerVolume = toTrigger[player].Dequeue();
-                                triggerVolume.Touch(GameManager.Instance.World, player);
-                            }
-                            else
-                            {
-                                toTrigger[player].Clear();
-                                toTrigger.Remove(player);
-                                toSpawn[player].Clear();
-                                poiTracker.Remove(player);
-                                toSpawn.Remove(player);
-                            }
-                        }
-                        else
-                        {
-                            QueueSleeperVolumesForSpawn(player);
                         }
                     }
+                    poiSpawnTracking.RemoveAll(p => p.LastPlayerInPOI < DateTime.Now.AddSeconds(-30));
 
+                    foreach (var poi in poiSpawnTracking)
+                    {
+                        // check if there are any sleeper volumes left to spawn
+                        if (poi.SleeperVolumes.Count > 0)
+                        {
+                            foreach (var player in GameManager.Instance.World.Players.list)
+                            {
+                                // if player is not null and within spawn radius, spawn sleeper volumes
+                                if (player != null && poi.POIPrefab.Equals(player.prefab))
+                                {
+                                    var toSpawn = poi.SleeperVolumes
+                                        .Where(s => !s.wasCleared && CheckDistance(s.Center, player));
+
+                                    foreach (var sleeperVolume in toSpawn)
+                                    {
+                                       sleeperVolume.TouchGroup(GameManager.Instance.World, player, Config.Instance.SpawnAggressive);
+                                    }
+                                }
+                            }
+                        }
+                        
+                        // check for trigger volumes
+                        if (poi.TriggerVolumes.Count > 0)
+                        {
+                            var trigger = poi.TriggerVolumes.Dequeue();
+                            if (!trigger.isTriggered)
+                            {
+                                trigger.Touch(GameManager.Instance.World, poi.MostRecentPlayer);
+                            }
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
                     Logging.LogOnce(ex.Message);
                 }
-
-                yield return new WaitForSeconds(0.25f);
+                yield return new WaitForSeconds(0.5f);
             }
-        }        
+        }
 
-        public void QueueSleeperVolumesForSpawn(EntityPlayer player)
+        private void QueueNewPOIs()
         {
-            if (player == null || player.prefab == null)
-                return;
-
-            if (toSpawn.ContainsKey(player))
+            foreach (var player in GameManager.Instance.World.Players.list)
             {
-                toSpawn[player].Clear();
-                toSpawn.Remove(player);
-            }
+                // is player inside a POI?
+                if (player.prefab != null)
+                {
+                    var poi = poiSpawnTracking.FirstOrDefault(p => p.POIPrefab.Equals(player.prefab));
+                    if (poi == null)
+                    {
+                        poi = new POISpawnTracking
+                        {
+                            POIPrefab = player.prefab,
+                            SleeperVolumes = new List<SleeperVolume>(),
+                            TriggerVolumes = new Queue<TriggerVolume>(),
+                            LastPlayerInPOI = DateTime.Now,
+                            MostRecentPlayer = player
+                        };
 
-            if (toTrigger.ContainsKey(player))
+                        poi.SleeperVolumes = player.prefab.sleeperVolumes
+                            .Where(s => !s.wasCleared && !IsInsideLandClaimBlock(s.Center))
+                            .OrderBy(s => Vector3.Distance(s.Center, player.position))
+                            .ToList();
+
+                        var triggers = player.prefab.triggerVolumes
+                            .Where(t => !t.isTriggered)
+                            .OrderBy(t => Vector3.Distance(t.Center, player.position));
+
+                        foreach (var trigger in triggers)
+                        {
+                            poi.TriggerVolumes.Enqueue(trigger);
+                        }
+
+                        poiSpawnTracking.Add(poi);
+                    }
+                    else
+                    {
+                        poi.LastPlayerInPOI = DateTime.Now;
+                        poi.MostRecentPlayer = player;
+                    }
+                }
+            }
+        }
+
+        private bool IsInsideLandClaimBlock(Vector3 center)
+        {
+            return false;
+        }
+
+        private static bool CheckDistance(Vector3 point, EntityPlayer player)
+        {
+            if (Config.Instance.UseSplitSpawnRadii)
             {
-                toTrigger[player].Clear();
-                toTrigger.Remove(player);
+                if (Math.Abs(point.y - player.position.y) <= Config.Instance.VerticalSpawnRadius)
+                {
+                    if (Vector2.Distance(new Vector2(point.x, point.z), new Vector2(player.position.x, player.position.z)) <= Config.Instance.SpawnRadius)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
             }
-
-            toSpawn.Add(player, new Queue<SleeperVolume>());
-            toTrigger.Add(player, new Queue<TriggerVolume>());
-
-            if (poiTracker.ContainsKey(player))
-                poiTracker[player] = player.prefab;
             else
-                poiTracker.Add(player, player.prefab);
-
-            foreach (var sleeperVolume in player.prefab.sleeperVolumes
-                .Where(s => !s.wasCleared)
-                .OrderBy(s => Vector3.Distance(s.Center, player.position)))
             {
-                toSpawn[player].Enqueue(sleeperVolume);
+                return Vector3.Distance(point, player.position) <= Config.Instance.SpawnRadius;
             }
-
-            foreach (var triggerVolume in player.prefab.triggerVolumes
-                .Where(t => !t.isTriggered)
-                .OrderBy(t => Vector3.Distance(t.Center, player.position)))
-            {
-                toTrigger[player].Enqueue(triggerVolume);
-            }
-        }
-
-        internal void ResetSleeperVolumes(EntityPlayer player)
-        {
-            if (player == null || player.prefab == null)
-                return;
-
-            foreach (var sleeperVolume in player.prefab.sleeperVolumes)
-            {
-                sleeperVolume.Reset();
-            }
-        }
+        }     
     }
 
     [HarmonyPatch(typeof(global::GameManager))]
@@ -169,10 +204,7 @@ namespace SpawnSleepersInRange.Harmony
     {
         public static void Postfix()
         {
-            if (Config.Instance.SpawningMethod == SpawningMethod.POI)
-            {
-                SpawnManager.Instance.Start();
-            }
+            SpawnManager.Instance.Start();
         }
     }
 
@@ -182,10 +214,7 @@ namespace SpawnSleepersInRange.Harmony
     {
         public static void Postfix()
         {
-            if (Config.Instance.SpawningMethod == SpawningMethod.POI)
-            {
-                SpawnManager.Instance.Stop();
-            }
+            SpawnManager.Instance.Stop();
         }
     }
 }
