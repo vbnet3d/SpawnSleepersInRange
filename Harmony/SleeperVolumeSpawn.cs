013351b5a52e9abac8eb7c56a6918c88045e1a5a
vbnet3d <ddzimianskigmail.com>
2025-07-04 18:32:30 -0400

    Update respawn time when SpawnAggressive is set to true

--- Harmony/SleeperVolumeSpawn.cs
+++ Harmony/SleeperVolumeSpawn.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using SpawnSleepersInRange.Common;
 using System;
+using System.Collections.Generic;
 
 namespace SpawnSleepersInRange.Harmony
 {
@@ -8,7 +9,7 @@ namespace SpawnSleepersInRange.Harmony
     [HarmonyPatch("Spawn")]
     public class SleeperVolumeSpawn
     {
-        public static void Postfix(ref EntityAlive __result, int ___flags)
+        public static void Postfix(SleeperVolume __instance, ref EntityAlive __result, int ___flags)
         {
             if (__result == null)
             {
@@ -19,13 +20,51 @@ namespace SpawnSleepersInRange.Harmony
             {
                 __result.SetSleeperActive();
                 __result.ResumeSleeperPose();
-
+                __instance.respawnTime = Math.Max(__instance.respawnTime, GameManager.Instance.World.worldTime + 1000);
                 __result.IsSleeperPassive = !Config.Instance.SpawnAggressive;
             }
             catch (Exception ex)
             {
                 Log.Out("SpawnSleepersInRange::SleeperVolumeSpawn failed: " + ex.Message);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(global::SleeperVolume))]
+    [HarmonyPatch("Reset")]
+    public class SleeperVolumeReset
+    {
+        public static bool Prefix(SleeperVolume __instance)
+        {
+            if (!IsInLandClaim(__instance))
+                Reset(__instance);
+
+            return false;
+        }
+
+        private static void Reset(SleeperVolume instance)
+        {
+            instance.playerTouchedToUpdate = (EntityPlayer)null;
+            instance.playerTouchedTrigger = (EntityPlayer)null;
+            instance.respawnTime = ulong.MaxValue;
+            instance.isSpawning = false;
+            instance.isSpawned = false;
+            instance.wasCleared = false;
+            instance.numSpawned = 0;
+            instance.respawnMap.Clear();
+            instance.respawnList = (List<int>)null;
+            if (instance.minScript == null)
+                return;
+            instance.minScript.Reset();
+        }
+
+        private static bool IsInLandClaim(SleeperVolume instance)
+        {
+            //var pos = new Vector3i(instance.Center.x, instance.Center.y, instance.Center.z);
+            //var chunk = GameManager.Instance.World.GetChunkFromWorldPos(pos);
+            //var lcbs = GameManager.Instance.World.IsLandProtectedBlock(chunk, pos, )
+
+            return false;
         }
     }
 }
